779e97586ef3ed8a6bf3ab0435b71e6fdb3b4ab7
augustineonu <onuaugustine07@gmail.com>
2024-06-25 14:02:20 +0100

    fixed name for ios notification for ios screen legnth for car listing

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Augustine Onu on 25/06/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
