3a590414f2bb0bc85377190401e0a382c3b2c322
augustineonu <onuaugustine07@gmail.com>
2024-07-31 20:51:02 +0100

    fixed internet connection snackbar

--- lib/services/network_controller.dart
+++ lib/services/network_controller.dart
@@ -1,11 +1,19 @@
+import 'dart:async';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gti_rides/services/logger.dart';
+import 'package:gti_rides/services/notification_service.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class NetworkController extends GetxController {
+  Logger logger = Logger("Network Controller::");
   final Connectivity _connectivity = Connectivity();
   late final Dio _dio;
+  late StreamSubscription iSubscription;
+  bool isDeviceConnected = false;
 
   @override
   void onInit() {
@@ -24,50 +32,77 @@ class NetworkController extends GetxController {
         },
       ),
     );
-    _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+    // _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+    _updateConnectionStatus();
   }
 
-  Future<void> _updateConnectionStatus(
-      ConnectivityResult connectivityResult) async {
-    if (connectivityResult == ConnectivityResult.none) {
-      Get.rawSnackbar(
-          messageText: const Text('PLEASE CONNECT TO THE INTERNET',
-              style: TextStyle(color: Colors.white, fontSize: 14)),
-          isDismissible: false,
-          duration: const Duration(days: 1),
-          backgroundColor: Colors.red[400]!,
-          icon: const Icon(
-            Icons.wifi_off,
-            color: Colors.white,
-            size: 35,
-          ),
-          margin: EdgeInsets.zero,
-          snackStyle: SnackStyle.GROUNDED);
-    } else {
-      // internet connection is available, check if Google.com is reachable to return stats 200
-      try {
-        final response = await _dio.get('https://www.google.com');
-        if (response.statusCode != 200 || response.statusCode != 201) {
-          Get.rawSnackbar(
-              messageText: const Text('PLEASE CONNECT TO THE INTERNET',
-                  style: TextStyle(color: Colors.white, fontSize: 14)),
-              isDismissible: false,
-              duration: const Duration(days: 1),
-              backgroundColor: Colors.red[400]!,
-              icon: const Icon(
-                Icons.wifi_off,
-                color: Colors.white,
-                size: 35,
-              ),
-              margin: EdgeInsets.zero,
-              snackStyle: SnackStyle.GROUNDED);
+  Future<void> _updateConnectionStatus() async {
+    iSubscription = Connectivity().onConnectivityChanged.listen((event) async {
+      isDeviceConnected = await InternetConnectionChecker().hasConnection;
+      // if(mounted){
+
+      // }
+      if (!isDeviceConnected) {
+        Get.rawSnackbar(
+            messageText: const Text('PLEASE CONNECT TO THE INTERNET',
+                style: TextStyle(color: Colors.white, fontSize: 14)),
+            isDismissible: false,
+            duration: const Duration(days: 1),
+            backgroundColor: Colors.red[400]!,
+            icon: const Icon(
+              Icons.wifi_off,
+              color: Colors.white,
+              size: 35,
+            ),
+            margin: EdgeInsets.zero,
+            snackStyle: SnackStyle.GROUNDED);
+      } else {
+        logger.log("Connected:: ");
+        if (Get.isSnackbarOpen) {
+          Get.closeCurrentSnackbar();
         }
-      } catch (exception) {
-        throw 'An error occurred';
       }
-      if (Get.isSnackbarOpen) {
-        Get.closeCurrentSnackbar();
-      }
-    }
+    });
+
+    // if (connectivityResult == ConnectivityResult.none) {
+    //   Get.rawSnackbar(
+    //       messageText: const Text('PLEASE CONNECT TO THE INTERNET',
+    //           style: TextStyle(color: Colors.white, fontSize: 14)),
+    //       isDismissible: false,
+    //       duration: const Duration(days: 1),
+    //       backgroundColor: Colors.red[400]!,
+    //       icon: const Icon(
+    //         Icons.wifi_off,
+    //         color: Colors.white,
+    //         size: 35,
+    //       ),
+    //       margin: EdgeInsets.zero,
+    //       snackStyle: SnackStyle.GROUNDED);
+    // } else {
+    //   // internet connection is available, check if Google.com is reachable to return stats 200
+    //   try {
+    //     final response = await _dio.get('https://www.google.com');
+    //     if (response.statusCode != 200 || response.statusCode != 201) {
+    //       Get.rawSnackbar(
+    //           messageText: const Text('PLEASE CONNECT TO THE INTERNET',
+    //               style: TextStyle(color: Colors.white, fontSize: 14)),
+    //           isDismissible: false,
+    //           duration: const Duration(days: 1),
+    //           backgroundColor: Colors.red[400]!,
+    //           icon: const Icon(
+    //             Icons.wifi_off,
+    //             color: Colors.white,
+    //             size: 35,
+    //           ),
+    //           margin: EdgeInsets.zero,
+    //           snackStyle: SnackStyle.GROUNDED);
+    //     }
+    //   } catch (exception) {
+    //     throw 'An error occurred';
+    //   }
+    //   if (Get.isSnackbarOpen) {
+    //     Get.closeCurrentSnackbar();
+    //   }
+    // }
   }
 }
